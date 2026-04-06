c398e5ee50b7e27ef02f03aa4fc4cae2073659ba
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-04-06 00:14:07 -0500

    Update build/bench/CTestTestfile.cmake

---
+++ build/bench/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/luisroayerdi/Coding/Matching-Engine/Matching-Engine
+# Build directory: /Users/luisroayerdi/Coding/Matching-Engine/Matching-Engine/build/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/luisroayerdi/Coding/Matching-Engine/Matching-Engine/build/bench/unit_tests[1]_include.cmake")
+subdirs("_deps/googletest-build")
+subdirs("_deps/googlebenchmark-build")
